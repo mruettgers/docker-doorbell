c4e4627278d7bdeb125a406e1eaa23d4a9a1044f
Michael Rüttgers <michael.ruettgers@lpr-it.de>
2019-12-24 14:33:27 +0100

    Switching to buildx

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 set -xe
 
 # Build Raspberry Pi image
-docker build -t mruettgers/rpi-doorbell -f targets/rpi/Dockerfile .
+docker buildx build --platform=linux/arm/v7 -t mruettgers/doorbell -f targets/rpi/Dockerfile .
+#docker build -t mruettgers/rpi-doorbell -f targets/rpi/Dockerfile .
