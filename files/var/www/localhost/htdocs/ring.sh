3d14f0910b5dad1893c487d4974c1d890a587f61
Michael Ruettgers <michael@ruettgers.eu>
2019-12-24 09:32:25 +0100

    Allow parameterization of sound file and sink

--- files/var/www/localhost/htdocs/ring.sh
+++ files/var/www/localhost/htdocs/ring.sh
@@ -1,12 +1,18 @@
-#!/bin/ash
+#!/usr/bin/haserl
+content-type: text/plain
+
+OK
+<%
 
 export $(cat /doorbell/.env | xargs)
 
-echo "OK"
+PULSE_SINK=${GET_pulse_sink:-${PULSE_SINK}}
+DOORBELL_SOUND=${GET_doorbell_sound:-${DOORBELL_SOUND}}
 
 if [ ! -z "${PULSE_SINK}" ]; then
-  /usr/bin/paplay -d ${PULSE_SINK} ${DOORBELL_SOUND} &
+  /usr/bin/paplay -d ${PULSE_SINK} ${DOORBELL_SOUND} &      
 else
-  /usr/bin/paplay ${DOORBELL_SOUND} &
+  /usr/bin/paplay ${DOORBELL_SOUND} &     
 fi
 
+%>
