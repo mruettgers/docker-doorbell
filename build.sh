8d2c7c621451ae6bfb8eb28208167fbce27b545d
Michael Ruettgers <michael@ruettgers.eu>
2019-12-25 23:56:17 +0100

    Added push helper

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -xe
 
 # Build Raspberry Pi image
-docker buildx build --platform=linux/arm/v7 -t mruettgers/doorbell -f targets/rpi/Dockerfile .
+docker buildx build --platform=linux/arm/v7 -t mruettgers/doorbell -f targets/rpi/Dockerfile . $@
